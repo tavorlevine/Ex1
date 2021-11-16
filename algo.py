74bd4e5b28be8fea59f2268210569618017335df
chenshtynmetz <jiayhb@gmail.com>
2021-11-16 14:13:25 +0200

    commit16/11-14:13

--- algo.py
+++ algo.py
@@ -1,4 +1,6 @@
 import csv
+import string
+
 from Elevator import Elevator
 from CallForElevator import CallForElevator
 from Building import Building
@@ -6,44 +8,62 @@ from listCalls import listCalls
 import math
 
 
-def allocate(self, calls: listCalls, file):
-    for c in calls:
-        if self.Building.count == 1:
-            c.elev = 0
-        else:
-            time = 9999999
-            tempelev = -1
-            for elev in Building.elevators:
-                if elev.list_c.count() == 0:
-                    elevtime = self.timeFromTo(c, elev)
-                    if elevtime < time:
-                        time = elevtime
-                        tempelev = elev.id
-                else:
-                    if elev.list_c[-1].time + self.timeOneCall(elev.list_c[len(elev.list_c) - 2].dest, elev.list_c[-1],
-                                                               elev) < c.time:
-                        elevtime = self.timeOneCall(elev.list_c[-1].dest, c, elev)
+class algo:
+
+    def __init__(self, building: string, calls: string, out: string):
+        # self.build = building
+        # self.calls = calls
+        self.output = out
+        self.list = listCalls(calls)
+        self.b = building(building)
+
+    # if __name__ == "__main__":
+    #     alg = algo(r"C:\Users\חן שטינמץ\PycharmProjects\Ex1\data\Ex1_input\Ex1_Calls\Calls_a.csv")
+    #     calls = listCalls(file_name)
+    #     allocate(calls)
+    # if __name__ == '__main__':
+    #    al = algo(r"C:\Users\חן שטינמץ\PycharmProjects\Ex1\data\Ex1_input\Ex1_Buildings\B1.json", r"C:\Users\חן שטינמץ\PycharmProjects\Ex1\data\Ex1_input\Ex1_Calls\Calls_b.csv", r"out.csv")
+    #    al.allocate(al.list)
+
+    def allocate(self, calls: listCalls, file):
+        for c in calls:
+            if self.Building.count == 1:
+                c.elev = 0
+            else:
+                time = 9999999
+                tempelev = -1
+                for elev in Building.elevators:
+                    if elev.list_c.count() == 0:
+                        elevtime = self.timeFromTo(c, elev)
                         if elevtime < time:
                             time = elevtime
                             tempelev = elev.id
                     else:
-                        list_2 = elev.list_c.reverse
-                        for c2 in list_2:
-                            if c2.time + self.timeOneCall(elev.list_c[c2.id - 1].dest, c2, elev) < c.time:
-                                elevtime = self.timeOneCall(c2.dest, c, elev)
-                                if elevtime < time:
-                                    time = elevtime
-                                    tempelev = elev.id
-                                break
-                            elif c2.time < c.time:
-                                sumOfCall = len(elev.list_c) - elev.list_c.index(c2)
-                                elevtime = self.timeSumCalls(sumOfCall, c2.dest, c, elev)
-                                if elevtime < time:
-                                    time = elevtime
-                                    tempelev = elev.id
-                                break
-        c.elev = tempelev
-    calls.writeToCSV(calls.file)
+                        if elev.list_c[-1].time + self.timeOneCall(elev.list_c[len(elev.list_c) - 2].dest,
+                                                                   elev.list_c[-1],
+                                                                   elev) < c.time:
+                            elevtime = self.timeOneCall(elev.list_c[-1].dest, c, elev)
+                            if elevtime < time:
+                                time = elevtime
+                                tempelev = elev.id
+                        else:
+                            list_2 = elev.list_c.reverse
+                            for c2 in list_2:
+                                if c2.time + self.timeOneCall(elev.list_c[c2.id - 1].dest, c2, elev) < c.time:
+                                    elevtime = self.timeOneCall(c2.dest, c, elev)
+                                    if elevtime < time:
+                                        time = elevtime
+                                        tempelev = elev.id
+                                    break
+                                elif c2.time < c.time:
+                                    sumOfCall = len(elev.list_c) - elev.list_c.index(c2)
+                                    elevtime = self.timeSumCalls(sumOfCall, c2.dest, c, elev)
+                                    if elevtime < time:
+                                        time = elevtime
+                                        tempelev = elev.id
+                                    break
+            c.elev = tempelev
+        calls.writeToCSV(self.output)
 
 
 def timeFromTo(self, c: CallForElevator, elev: Elevator):
@@ -64,5 +84,6 @@ def timeOneCall(self, pos: int, c: CallForElevator, elev: Elevator):
 def timeSumCalls(self, sum_2: int, pos: int, c: CallForElevator, elev: Elevator):
     stages = abs(pos - c.getSrc())
     stages = stages + abs(c.src - c.dest)
-    elevtime = (elev.speed / stages) + (elev.stopTime + elev.startTime + elev.openTime) * sum_2 + elev.closeTime * (sum_2 - 1)
+    elevtime = (elev.speed / stages) + (elev.stopTime + elev.startTime + elev.openTime) * sum_2 + elev.closeTime * (
+            sum_2 - 1)
     return elevtime
--- listCalls.py
+++ listCalls.py
@@ -2,7 +2,7 @@ import csv
 from Elevator import Elevator
 from CallForElevator import CallForElevator
 from Building import Building
-
+# from algo import algo
 
 
 class listCalls:
@@ -20,22 +20,23 @@ class listCalls:
             with open(fileName) as file:
                 csvreader = csv.reader(file)
                 for row in csvreader:
-                    c = CallForElevator(header=row[0], time=float(row[1]), src=int(row[2]), dest=int(row[3]), parmeter=int(row[4]), elev=int(row[5]))
+                    c = CallForElevator(header=row[0], time=float(row[1]), src=int(row[2]), dest=int(row[3]),
+                                        parmeter=int(row[4]), elev=int(row[5]))
                     cl.append(c)
                     # rows.append(row)
                     self.count = self.count + 1
                 self.calls = cl
-                for i in self.calls:
-                    print(i.toString())
+                # for i in self.calls:
+                #     print(i.toString())
         except IOError as e:
             print(e)
 
-    def writeToCSV(self):
+    def writeToCSV(self, out):
         new_calls = []
         for team in self.calls:
             new_calls.append(team.__dict__.values())
         try:
-            with open("out.csv", 'w', newline="") as file:
+            with open(out, 'w', newline="") as file:
                 csvwriter = csv.writer(file)
                 csvwriter.writerows(new_calls)
         except IOError as e:
@@ -56,6 +57,7 @@ class listCalls:
 if __name__ == '__main__':
     l1 = listCalls(r"C:\Users\חן שטינמץ\PycharmProjects\Ex1\data\Ex1_input\Ex1_Calls\Calls_b.csv")
     # print(l1)
-    l1.calls[0].elev = 23
-    l1.writeToCSV()
+    # l1.calls[0].elev = 23
+    # l1.writeToCSV()
+    # l1.allocate(l1)
 
